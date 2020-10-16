c146f953d3f3ed419220c778ddf324495a449278
Anh Ton <anhton@Anhs-MacBook-Air.local>
2020-10-16 11:13:30 -0400

    commit

--- shiny-app/server.R
+++ shiny-app/server.R
@@ -11,12 +11,90 @@ library(shiny)
 library(shinythemes)
 library(tidyverse)
 library(ggplot2)
+library(readxl)
+library(janitor)
 
+juvenile_stats_2014 <- read_excel("Juvenile_stats_2014.xlsx") %>%
+  clean_names() %>% 
+  select(!total) %>%
+  mutate(year = 2014)
+
+juvenile_stats_2015 <- read_excel("Juvenile_stats_2015.xlsx") %>%
+  clean_names() %>% 
+  select(!grand_total) %>%
+  rename(barnstable = barnstable_town_of_plymouth) %>%
+  mutate(year = 2015)
+
+juvenile_stats_2016 <- read_excel("Juvenile_stats_2016.xlsx") %>%
+  clean_names() %>%
+  select(!total) %>%
+  rename(barnstable = barnstable_town_of_plymouth) %>%
+  mutate(year = 2016) %>%
+  mutate(suffolk = round(suffolk),
+         bristol = round(bristol),
+         essex = round(essex),
+         hampden = round(hampden),
+         middlesex = round(middlesex),
+         worcester = round(worcester))
+
+juvenile_stats_2017 <- read_excel("Juvenile_stats_2017.xlsx") %>%
+  clean_names() %>%
+  select(!total) %>%
+  rename(barnstable = barnstable_town_of_plymouth) %>%
+  mutate(suffolk = round(suffolk),
+         bristol = round(bristol),
+         essex = round(essex),
+         hampden = round(hampden),
+         middlesex = round(middlesex),
+         worcester = round(worcester))
+
+juvenile_stats_2018 <- read_excel("Juvenile_stats_2018.xlsx") %>%
+  clean_names() %>%
+  select(!total) %>%
+  rename(barnstable = barnstable_town_of_plymouth) %>%
+  mutate(year = 2018) %>%
+  mutate(suffolk = round(suffolk),
+         bristol = round(bristol),
+         essex = round(essex),
+         hampden = round(hampden),
+         middlesex = round(middlesex),
+         worcester = round(worcester))
+
+juvenile_stats_2019 <- read_excel("Juvenile_stats_2019.xlsx") %>%
+  clean_names() %>%
+  select(!total) %>%
+  rename(barnstable = barnstable_town_of_plymouth) %>%
+  mutate(year = 2019) %>%
+  mutate(suffolk = round(suffolk),
+         bristol = round(bristol),
+         essex = round(essex),
+         hampden = round(hampden),
+         middlesex = round(middlesex),
+         worcester = round(worcester))
+
+juvenile_6_years <- 
+  bind_rows(list(juvenile_stats_2014, juvenile_stats_2015,
+                 juvenile_stats_2016, juvenile_stats_2017,
+                 juvenile_stats_2018, juvenile_stats_2019),
+            .id = "year") %>% 
+  mutate(year = as.numeric(year) + 2013) %>%
+  group_by(year)
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
 
-    output$carPlot <- renderPlot({
-       ggplot(mtcars, aes(x = mpg, y = disp)) + geom_point()
+shinyServer(function(input, output) {
+    output$juvenile_case_type <- renderPlot({
+       ggplot(juvenile_stats_2014, 
+              aes(x = case_type, y = middlesex)) +
+        coord_flip() +
+        geom_col(fill = "navy") + 
+        labs(
+        title = "Cases brought to Juvenile Courts in Middlesex County, MA",
+              x = " ",
+              y = "Number of cases per kind") +
+        theme_bw() +
+        theme(plot.title = element_text(size = 18),
+              axis.text = element_text(size = 12))
+        
     })
 
 })
--- shiny-app/ui.R
+++ shiny-app/ui.R
@@ -11,22 +11,63 @@ library(shiny)
 library(shinythemes)
 library(tidyverse)
 library(ggplot2)
+library(readxl)
+library(janitor)
 
 
 # Define UI for application that draws a histogram
-shinyUI(navbarPage(theme = shinytheme("slate"),
-                   "Efficiency and Equity in the Courtroom", 
-    # Application title
-    tabPanel("Middlesex",
-             p("Hello World!", 
-             a("Berkshire DA Data", 
-    href = "https://www.dropbox.com/sh/fw4xfk02iiewvy2/AAASf7RBIKbbK8m2eT0WpKOea?dl=0" )),
-             p(a("Middlesex County", 
-                 href = "https://www.middlesexda.com/public-information/pages/prosecution-data-and-statistics")
-               )),
-    tabPanel("Berkshire", plotOutput("carPlot"))
-    ))
-    #more information HERE: https://www.w3schools.com/TAGS/default.ASP 
+shinyUI(navbarPage(theme = shinytheme("flatly"),
+                   "Racial Disparities in 
+                   Juvenile Courts, MA", 
+   
+# Application title
+   
+ tabPanel("Overview",
+        h2("About This Project"),
+        p("The ongoing Black Live Matters movement and my internship 
+        with an organization doing advocacy work for racial equity in
+        the juvenile legal system has got me interested in learning 
+        more about the issue. I plan to use the data from the official
+        Commonwealth of Massachusetts website for the year of 2014 - 2019
+        to look at some basic statistical background in the types and
+        volume of cases brought to the juvenile courts. I also want to 
+        investigate the prosecution data for Middlesex and Berkshire -
+        the two datasets that I currently have access to - to find some
+        insights regarding court efficiency (how long they pursue a case
+        and whether the case is dropped or resolved) and racial equity 
+        (whether the length of time from case filed to case resolution, 
+        case charge, and sentence types are systematically different for
+        people belonging to different races."),
+        br(),
+        br(),
+        h3("Note"),
+        p("I did some cleaning for the court data from mass.gov, but I have 
+        not processed the Middlesex and Berkshire data yet because I am 
+        still waiting for my supervisor's clarifications of many 
+        abbreviations used in the Middlesex and especially the Berkshire 
+        datasets, some technical terms, and what other information would be
+        helpful to be included."),
+        br(),
+        br(),
+        ),
+    
+    #Output 
+  tabPanel("Middlesex County",
+        h2("Test"),
+        p("This graph is just a shiny test and to fulfill the requirements
+        of Milestone #4 as I still need more time to do some research and 
+        think about what datasets are helpful to be put together to
+        shed light on my questions of whether the courts are efficient and
+        just, whether children of color are disproportionately involved 
+        in the justice system, and why such is the case (Is that racial
+        bias from the those enforcing and interpreting the laws? Economic
+        inequality? What are other possible factors am I missing?"),
+      
+        fluidPage(plotOutput("juvenile_case_type")
+    )
+))
+)
+#more information HERE: https://www.w3schools.com/TAGS/default.ASP 
     
 
 
